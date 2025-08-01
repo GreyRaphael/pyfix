ee7d1e1f74d0f87b86d4e2e3deed67e644ee3371
KTF41_gewei <grey@pku.edu.cn>
2025-08-01 14:58:17 +0800

    wiht order send

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import quickfix as fix
+import quickfix42 as fix42
 
 
 class Application(fix.Application):
@@ -12,6 +13,7 @@ class Application(fix.Application):
 
     def onLogon(self, sessionID):
         print(f"Logon: {sessionID}")
+        self.send_order()
 
     def onLogout(self, sessionID):
         print(f"Logout: {sessionID}")
@@ -27,6 +29,24 @@ class Application(fix.Application):
 
     def fromApp(self, message, sessionID):
         print(f"From App: {message}")
+
+    def send_order(self):
+        order = fix42.NewOrderSingle()
+
+        # 设置字段
+        order.setField(fix.ClOrdID("ORDER_001"))
+        order.setField(fix.HandlInst(fix.HandlInst_AUTOMATED_EXECUTION_ORDER_PRIVATE_NO_BROKER_INTERVENTION))  # 1 = 自动执行，代表 Best Execution
+        order.setField(fix.Symbol("AAPL"))
+        order.setField(fix.Side(fix.Side_BUY))  # 1 = Buy
+        order.setField(fix.TransactTime())
+        order.setField(fix.OrdType("2"))  # 2 = Limit
+
+        order.setField(fix.OrderQty(100))
+        order.setField(fix.Price(150.50))
+        order.setField(fix.TimeInForce("0"))  # 0 = Day
+
+        fix.Session.sendToTarget(order, self.sessionID)
+        print("Order sent.")
 
 
 if __name__ == "__main__":
